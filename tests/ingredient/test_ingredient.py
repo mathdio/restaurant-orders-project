42116fe56d58fc86dd475be3f4c559c58cbc8ac8
Matheus Diógenes <mathdiogenes@gmail.com>
2023-06-16 14:31:59 -0300

    feat: R01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,18 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    meat = Ingredient("carne")
+    assert meat == Ingredient("carne")
+    assert meat.name == "carne"
+    assert meat.__repr__() == "Ingredient('carne')"
+    assert meat.__hash__() == Ingredient("carne").__hash__()
+    assert meat.__hash__() != Ingredient("ovo").__hash__()
+    assert meat.restrictions == {
+        Restriction.ANIMAL_DERIVED,
+        Restriction.ANIMAL_MEAT,
+    }
