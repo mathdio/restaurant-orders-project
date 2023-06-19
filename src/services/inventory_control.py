c8345fbce499c7b06357b454a85a23b2d6c184eb
Matheus Diógenes <mathdiogenes@gmail.com>
2023-06-19 15:24:23 -0300

    feat: R05

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -27,8 +27,19 @@ class InventoryMapping:
 
     # Req 5.1
     def check_recipe_availability(self, recipe: Recipe):
-        pass
+        for ingredient in recipe:
+            if (
+                ingredient not in self.inventory
+                or self.inventory[ingredient] < recipe[ingredient]
+            ):
+                return False
+        return True
 
     # Req 5.2
     def consume_recipe(self, recipe: Recipe) -> None:
-        pass
+        if self.check_recipe_availability(recipe):
+            for ingredient in recipe:
+                self.inventory[ingredient] -= recipe[ingredient]
+            return None
+        else:
+            raise ValueError()
